2c5adcfd11841f17e03ae4b69e944456773a2f58
Aarti <arati.verma@taskcraft.in>
2023-08-07 18:49:26 +0530

    First commit

---
+++ db/migrate/20230807061914_create_courses.rb
@@ -0,0 +1,13 @@
+class CreateCourses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :courses do |t|
+      t.string :title
+      t.string :description
+      t.decimal :price
+      t.references :category, null: false, foreign_key: true
+      t.references :teacher, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
